38c1c524a53270473ac2b1c7c1de82a86312a8dd
Marko Rrokaj <rrokaj.marko@gmail.com>
2024-10-09 04:22:57 +0100

    MongoDB Empolyment and Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<RegisterForm />}></Route>
+        <Route path="/login" element={<LoginForm />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,17 +1,50 @@
 import "./LoginForm.css";
+import { useState, FormEvent } from "react";
+import { useNavigate } from "react-router-dom"; // Updated to ensure correct import for React Router
 import { FaUser, FaLock } from "react-icons/fa";
+import axios from "axios";
 
-const LoginForm = () => {
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const navigate = useNavigate(); // Ensure to use the navigate function from useNavigate
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const result = await axios.post("http://localhost:5173/login", {
+        email,
+        password,
+      });
+      if (result.data === "Success") {
+        navigate("/home");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div className="wrapper">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <div className="input-box">
-          <input type="text" placeholder="Username" required />
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            value={email} // Assuming username is actually the email
+            onChange={(e) => setEmail(e.target.value)} // Capture input for email
+          />
           <FaUser className="icon" />
         </div>
         <div className="input-box">
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // Capture input for password
+          />
           <FaLock className="icon" />
         </div>
 
@@ -26,7 +59,7 @@ const LoginForm = () => {
         <button type="submit">Login</button>
         <div className="register-link">
           <p>
-            Don't have an account? <a href="#">Register</a>
+            Don't have an account? <a href="./register">Register</a>
           </p>
         </div>
       </form>
